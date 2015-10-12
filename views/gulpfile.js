ba88266e63ca0200d2cd3b94d04d9e089704377c
frandman <franjubrique@hotmail.com>
2015-10-12 20:24:21 +0200

    gulp file and minification

---
+++ views/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+
+gulp.task('scripts', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('styles', function() {
+  return gulp.src('src/css/*.css')
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/img/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('default', ['scripts', 'styles', 'images']);
