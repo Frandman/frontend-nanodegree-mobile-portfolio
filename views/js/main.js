86dfc44f523be8ad347c9c9bf0d5f78fed81a636
frandman <franjubrique@hotmail.com>
2015-10-12 17:08:27 +0200

    reduced the number of pizzas, forced layer creation on css, move using translatex to avoid painting

--- views/js/main.js
+++ views/js/main.js
@@ -491,7 +491,6 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
-
   frame++;
   window.performance.mark("mark_start_frame");
 /**   I've created a vriable scroll_ in order to calculate the scrollTop distance outside the loop*/
@@ -499,7 +498,8 @@ function updatePositions() {
   var scroll_ = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((scroll_ / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    var x_translate = 90 * phase;
+    items[i].style.transform = "translateX("+x_translate+"px)";
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
@@ -513,21 +513,28 @@ function updatePositions() {
 }
 
 // runs updatePositions on scroll
-window.addEventListener('scroll', updatePositions);
+window.addEventListener('scroll',updatePositions);
+function updatePizzas(){
+	requestAnimationFrame(updatePositions);
+}
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-  var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  var cols = Math.floor(window.innerWidth/s);
+  var rows = Math.floor(window.innerHeight/s);
+  console.log("rows: "+rows);
+  for (var i = 0; i <= cols; i++) {
+  	for (var j = 0; j <= rows; j++){
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
-    elem.basicLeft = (i % cols) * s;
-    elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    elem.style.left = i * s + 'px';
+    elem.style.top =  j * s + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
-  }
+    } 
+}
   updatePositions();
 });
